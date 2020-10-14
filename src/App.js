46740188f110e398595f2a39fd51dd964c28e7a8
Drews777 <57402415+Drews777@users.noreply.github.com>
2020-10-14 17:45:30 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
+import Features from './components/Features/Features'
+import Calendar from './components/Calendar/Calendar'
+import Details from './components/Details/Details'
+import Footer from './components/Footer/Footer'
 import './style.css';
 function App() {
   return (
     <>
     <Header />
     <Main />
+    <Features />
+    <Calendar />
+    <Details />
+    <Footer />
     </>
   );
 }
